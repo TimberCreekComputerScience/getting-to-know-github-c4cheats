16ee28f777391fb607cd57215fdd09f65adc4bc7
Kyle Dencker <kdencker@gmail.com>
2019-08-19 09:45:56 -0400

    Add files via upload

---
+++ WhatsUpGuys.java
@@ -0,0 +1,13 @@
+public class WhatsUpGuys {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		int nice = 4;
+		nice = 18 / 5;
+		nice = 18 % 5;
+		double fine = 18 / 5;
+		fine = 18.0 / 5;
+
+	}
+
+}
